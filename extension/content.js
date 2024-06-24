67d944200f0617052e94f282ee2cf71735db9e8a
Juan Llamas <38849891+xoltia@users.noreply.github.com>
2024-06-24 10:36:19 -0500

    Update content.js license notice

--- extension/content.js
+++ extension/content.js
@@ -1,8 +1,27 @@
-/*
-Copyright (c) 2022-2023 Michael Ren
-Licensing and distribution info can be found at the GitHub repository
-https://github.com/XFG16/YouTubeDiscordPresence
-*/
+// Copyright (c) 2022-2024 Michael Ren
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// ------------------------------- END OF LICENSE -------------------------------
+
+// This script is a modified version of the original script found in the
+// YouTubeDiscordPresence project (https://github.com/XFG16/YouTubeDiscordPresence).
 
 const MESSAGE_FREQUENCY = 1000;
 const AD_SELECTOR = "div.ytp-ad-player-overlay-instream-info"; // DOCUMENT; THIS HAS TO BE DONE BECAUSE IF AN AD PLAYS IN THE MIDDLE OF A VIDEO, THEN GETPLAYERSTATE WILL STILL RETURN 1
